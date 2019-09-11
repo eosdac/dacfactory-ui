1229a01dcc42e9d7e958bb197daa8469969a4483
unknown <kasperkasperfish@gmail.com>
2019-09-11 23:21:33 +0200

    sync

--- src/components/factory/stepper.vue
+++ src/components/factory/stepper.vue
@@ -1,16 +1,10 @@
 <template>
   <div>
     <q-linear-progress dark :value="getProgressValue" color="secondary" />
-    <!-- <div class="q-px-md q-pb-md row justify-between">
-      
-      <q-btn v-if="shouldDisplayPrevStepBtn" color="secondary" label="go back" class="q-mt-sm" icon="ion-arrow-back" @click="prevStep" />
-      <q-space v-else/>
 
-      <q-btn color="secondary" :label="`go to step ${getActiveStep+1}`" class="q-mt-sm" icon-right="ion-arrow-forward" @click="nextStep" />
-    </div> -->
     <div class="row  items-center">
       <div class="col-4 row justify-start q-pl-md q-pb-md">
-        <q-btn v-if="shouldDisplayPrevStepBtn" color="primary"  class="q-mt-sm"  @click="prevStep" >
+        <q-btn v-if="shouldDisplayPrevStepBtn" color="secondary"  class="q-mt-sm"  @click="prevStep" >
           <q-icon name="ion-arrow-back" />
           <div v-if="$q.screen.gt.xs" class="on-right">go back</div>
         </q-btn>
@@ -20,7 +14,7 @@
         <div v-else class="text-h5">Welcome</div>
       </div>
       <div class="col-4 row justify-end q-pr-md q-pb-md">
-        <q-btn color="primary"  class="q-mt-sm"  @click="nextStep" >
+        <q-btn color="secondary"  class="q-mt-sm"  @click="nextStep" >
           <div v-if="$q.screen.gt.xs" class="on-left">{{`go to step ${getActiveStep+1}`}}</div>
           <q-icon name="ion-arrow-forward" />
         </q-btn>
