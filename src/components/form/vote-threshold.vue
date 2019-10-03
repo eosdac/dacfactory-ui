e14ffdbd05d49b91d772bf7925df86d7903f1cd2
Nikita-Pobazhak <nikita.pobazhak@gmail.com>
2019-10-03 18:08:43 +0300

    high > med > low condition was added

--- src/components/form/vote-threshold.vue
+++ src/components/form/vote-threshold.vue
@@ -1,58 +1,54 @@
 <template>
   <div>
-    
     <div class=" row q-col-gutter-md ">
-
       <div class=" col-xs-4">
-        <integer-input v-model="thresholdHigh" label="High" :max="4"/>
+        <integer-input
+          v-model="thresholdHigh"
+          label="High"
+          :max="4"
+        />
       </div>
-
       <div class=" col-xs-4">
-        <integer-input v-model="thresholdMed" label="Med"/>
+        <integer-input
+          v-model="thresholdMed"
+          label="Med"
+          :max="thresholdHigh - 1 < 0 ? 0 : thresholdHigh - 1"
+        />
       </div>
-
       <div class=" col-xs-4">
-        <integer-input v-model="thresholdLow" label="Low" />
+        <integer-input
+          v-model="thresholdLow"
+          label="Low"
+          :max="thresholdMed - 1 < 0 ? 0 : thresholdMed - 1"
+        />
       </div>
-
     </div>
-
-    
   </div>
 </template>
 
 <script>
-import integerInput from 'components/form/integer-input'
-export default {
-  // name: 'ComponentName',
-  components:{
-    integerInput
-  },
-  data () {
-    return {
-      thresholdHigh:5,
-      thresholdMed:0,
-      thresholdLow:0,
-    }
-  },
-  methods:{
-    handleKeyDown($event){
-      let keycode = ($event.keyCode ? $event.keyCode : $event.which);
-      console.log(keycode)
-      if(keycode == 190 || keycode ==110 || keycode == 229){
-        $event.preventDefault();
-      }
+  import integerInput from "components/form/integer-input";
+
+  export default {
+    name: "VoteThreshold",
+    components: {
+      integerInput
     },
-    handleInput(e){
-      // thresholdHigh = $refs.test.$refs.input.valueAsNumber  = parseInt($event)
-      valueAsNumber
-      console.log(this.$refs.test.$refs)
-      console.log(e)
+    data() {
+      return {
+        thresholdHigh: 4,
+        thresholdMed: 0,
+        thresholdLow: 0
+      };
+    },
+    methods: {
+      handleKeyDown($event) {
+        let keycode = $event.keyCode ? $event.keyCode : $event.which;
+        console.log(keycode);
+        if (keycode === 190 || keycode === 110 || keycode === 229) {
+          $event.preventDefault();
+        }
+      }
     }
-
-  }
-}
+  };
 </script>
-
-<style>
-</style>
