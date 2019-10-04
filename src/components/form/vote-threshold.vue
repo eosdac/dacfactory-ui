1d888f5919f96340a44e5785d753345f0a4223d8
Nikita-Pobazhak <nikita.pobazhak@gmail.com>
2019-10-04 11:57:07 +0300

    layout fix

--- src/components/form/vote-threshold.vue
+++ src/components/form/vote-threshold.vue
@@ -1,55 +1,41 @@
 <template>
-  <div>
-    <div class=" row q-col-gutter-md ">
-      <div class=" col-xs-4">
-        <integer-input
-          v-model="thresholdHigh"
-          label="High"
-          :max="4"
-        />
-      </div>
-      <div class=" col-xs-4">
-        <integer-input
-          v-model="thresholdMed"
-          label="Med"
-          :max="checkMedMax"
-        />
-      </div>
-      <div class=" col-xs-4">
-        <integer-input
-          v-model="thresholdLow"
-          label="Low"
-          :max="checkLowMax"
-        />
-      </div>
+  <div class="row q-col-gutter-md">
+    <div class="col-xs-4">
+      <integer-input v-model="thresholdHigh" label="High" :max="4" />
+    </div>
+    <div class="col-xs-4">
+      <integer-input v-model="thresholdMed" label="Med" :max="checkMedMax" />
+    </div>
+    <div class="col-xs-4">
+      <integer-input v-model="thresholdLow" label="Low" :max="checkLowMax" />
     </div>
   </div>
 </template>
 
 <script>
-  import integerInput from "components/form/integer-input";
+import integerInput from "components/form/integer-input";
 
-  export default {
-    name: "VoteThreshold",
-    components: {
-      integerInput
+export default {
+  name: "VoteThreshold",
+  components: {
+    integerInput
+  },
+  data() {
+    return {
+      thresholdHigh: 4,
+      thresholdMed: 0,
+      thresholdLow: 0
+    };
+  },
+  computed: {
+    checkMedMax() {
+      const newMax = this.thresholdHigh - 1;
+      return newMax < 0 ? 0 : newMax;
     },
-    data() {
-      return {
-        thresholdHigh: 4,
-        thresholdMed: 0,
-        thresholdLow: 0
-      };
-    },
-    computed: {
-      checkMedMax() {
-        const newMax = this.thresholdHigh - 1;
-        return newMax < 0 ? 0 : newMax
-      },
-      checkLowMax() {
-        const newMax = this.thresholdMed - 1;
-        return newMax < 0 ? 0 : newMax
-      }
+    checkLowMax() {
+      const newMax = this.thresholdMed - 1;
+      return newMax < 0 ? 0 : newMax;
     }
-  };
+  }
+};
 </script>
