5aceb2af9ccec1b72e11c01d8b99a7e7ef7c60b0
Nikita-Pobazhak <nikita.pobazhak@gmail.com>
2019-11-04 14:52:20 +0300

    Add timeframe dropdown to period length #29

--- src/components/steps/Step3/Step3Form.vue
+++ src/components/steps/Step3/Step3Form.vue
@@ -17,7 +17,7 @@
       v-model="maxRequestPay"
       @statusChange="onStatusChange($event, 'maxRequestPay')"
     />
-    <div class="lockup-wrapper">
+    <div class="input-select-wrapper">
       <my-input
         type="number"
         color="secondary"
@@ -36,6 +36,7 @@
         @input="onStatusChange({ value: $event }, 'lockupSelect')"
       />
     </div>
+    <div class="input-select-wrapper">
     <my-input
       type="number"
       color="secondary"
@@ -45,6 +46,16 @@
       v-model="periodLength"
       @statusChange="onStatusChange($event, 'periodLength')"
     />
+      <q-select
+        outlined
+        dark
+        v-model="periodLengthSelect"
+        color="secondary"
+        :style="{ alignSelf: 'start' }"
+        :options="['Hour(s)', 'Day(s)']"
+        @input="onStatusChange({ value: $event }, 'periodLengthSelect')"
+      />
+    </div>
     <custom-divider />
     <my-input
       type="number"
@@ -82,6 +93,7 @@ export default {
       lockup: this.$store.state.factory.stepsData[3].lockup,
       lockupSelect: this.$store.state.factory.stepsData[3].lockupSelect,
       periodLength: this.$store.state.factory.stepsData[3].periodLength,
+      periodLengthSelect: this.$store.state.factory.stepsData[3].periodLengthSelect,
       numberElected: this.$store.state.factory.stepsData[3].numberElected,
       maxVotes: this.$store.state.factory.stepsData[3].maxVotes
     };
@@ -97,7 +109,7 @@ export default {
 <style scoped lang="stylus">
 .margin-bottom-14
   margin-bottom 14px
-.lockup-wrapper
+.input-select-wrapper
   display grid
   grid-template-columns 2fr 1fr
   grid-gap 28px
