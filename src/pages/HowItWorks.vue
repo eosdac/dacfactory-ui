246337cd45417836b8290f0d7b6d7f18359b276d
Michael Yeates <michaeljyeates@gmail.com>
2019-12-13 10:18:51 +0000

    Use correct filename for how it works page

--- src/pages/HowItWorks.vue
+++ src/pages/HowItWorks.vue
@@ -1,13 +1,39 @@
 <template>
-  <q-page padding>
-    <!-- content -->
-  </q-page>
+    <q-page class="bg-accent full-height">
+        <div class="bg-primary">
+            <div class="q-pa-md">
+
+                <p v-html="$t('how.line1')"></p>
+                <p v-html="$t('how.line2')"></p>
+
+                <hr />
+
+                <p v-html="$t('how.line3')"></p>
+
+                <blockquote>
+                    <p v-html="$t('how.line4')"></p>
+                    <p v-html="$t('how.line5')"></p>
+                    <p v-html="$t('how.line6')"></p>
+                </blockquote>
+
+                <p v-html="$t('how.line7')"></p>
+                <p v-html="$t('how.line8')"></p>
+                <p v-html="$t('how.line9')"></p>
+                <p v-html="$t('how.line10')"></p>
+                <p v-html="$t('how.line11')"></p>
+
+            </div>
+        </div>
+        <div class="diagonal"></div>
+    </q-page>
 </template>
 
 <script>
-export default {
-  // name: 'PageName',
-};
+    export default {
+        name: 'HowItWorks',
+    }
 </script>
 
-<style></style>
+<style>
+    a, a:hover, a:active, a:visited { color: white; }
+</style>
