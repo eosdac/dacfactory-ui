d9a6a437ba7c86fcaadb3fd8e45dc2444e2863f8
Michael Yeates <michaeljyeates@gmail.com>
2020-01-15 17:59:31 +0000

    Use content layout for how it works page

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,9 +6,8 @@ const routes = [
       { path: "", component: () => import("pages/Home.vue") },
       { path: "create/:step(step[1-5]+)", component: () => import("pages/Steps") },
       { path: "create/", redirect: "create/step1" },
-      { path: "how-it-works", component: () => import("pages/HowItWorks") },
-      { path: "terms", component: () => import("pages/Terms") },
       { path: "dac-creation", component: () => import("pages/DacCreation") },
+      { path: "terms", component: () => import("pages/Terms") },
       { path: "test", component: () => import("pages/Test") }
     ]
   },
@@ -16,6 +15,7 @@ const routes = [
     path: "/",
     component: () => import("layouts/ContentLayout"),
     children: [
+      { path: "how-it-works", component: () => import("pages/HowItWorks") },
       { path: "pricing", component: () => import("pages/Pricing") }
     ]
   }
