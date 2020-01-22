bb189607cd96548b670774fad702f0318c985eda
Michael Yeates <michaeljyeates@gmail.com>
2020-01-22 11:03:23 +0000

    Add i18n for how it works titles

--- src/pages/HowItWorks.vue
+++ src/pages/HowItWorks.vue
@@ -2,21 +2,21 @@
   <q-page class="full-height">
     <div class="bg-primary">
       <div class="q-pa-md">
-        <div class="text-h1">Creating your own DAC is now easy!</div>
+        <div class="text-h1">{{$t('how.intro_title')}}</div>
         <p v-html="$t('how.intro')"></p>
         <hr />
-        <div class="text-h2">How to start</div>
+        <div class="text-h2">{{$t('how.how_start_title')}}</div>
         <p v-html="$t('how.how_start1')"></p>
         <p v-html="$t('how.how_start2')"></p>
         <hr />
-        <div class="text-h2">I already have an existing EOS token</div>
+        <div class="text-h2">{{$t('how.existing_title')}}</div>
         <p v-html="$t('how.existing1')"></p>
-        <div class="text-h2">What happens after the DAC is created?</div>
+        <div class="text-h2">{{$t('how.what_happens_title')}}</div>
         <p v-html="$t('how.what_happens1')"></p>
         <p v-html="$t('how.what_happens2')"></p>
         <p v-html="$t('how.what_happens3')"></p>
         <p v-html="$t('how.what_happens4')"></p>
-        <div class="text-h2">Modifications</div>
+        <div class="text-h2">{{$t('how.modifications_title')}}</div>
         <p v-html="$t('how.modifications1')"></p>
         <p v-html="$t('how.modifications2')"></p>
         <hr />
