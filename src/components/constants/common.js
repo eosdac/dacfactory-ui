2daeffdf0a797f99efcc24f45a91ce3c4145916f
Michael Yeates <michaeljyeates@gmail.com>
2020-07-15 17:13:53 +0100

    Change max supply to 1bn

--- src/components/constants/common.js
+++ src/components/constants/common.js
@@ -3,7 +3,7 @@ export const STEPS_NUMBER = 5;
 export const SECONDS_IN_HOUR = 3600;
 
 ////////// steps' data
-export const MAX_SUPPLY_VALUE = 1000000000000;
+export const MAX_SUPPLY_VALUE = 1000000000;
 
 export const ERROR_MARK = "Error";
 
